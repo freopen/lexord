804615fff5f429067a60269d521da834f05fb5cf
Aleksei Zolotov <freopen@freopen.org>
2023-11-01 09:13:06 +0100

    Refactor single type into a newtype

--- fuzz/src/main.rs
+++ fuzz/src/main.rs
@@ -19,24 +19,57 @@ fn serialize_type<'a, T: LexOrd + Arbitrary<'a>>(
     Ok(result)
 }
 
+macro_rules! count {
+    () => (0usize);
+    ($head:tt $($tail:tt)*) => (1usize + count!($($tail)*));
+}
+
 macro_rules! gen_single_fns {
     ($($ty:ty)+) => {
-        #[allow(unused_assignments)]
-        fn test_single(
-            mut type_id: u16,
-            data: &mut Unstructured,
-            ser_a: &mut Vec<u8>,
-            ser_b: &mut Vec<u8>
-        ) -> arbitrary::Result<Option<Ordering>> {
-            $(
-                if type_id == 0 {
-                    let a: $ty = serialize_type(data, ser_a)?;
-                    let b: $ty = serialize_type(data, ser_b)?;
-                    return Ok(a.partial_cmp(&b));
+        struct Single(u16);
+
+        impl Single {
+            #[allow(unused_assignments)]
+            fn test_single(
+                &self,
+                data: &mut Unstructured,
+                ser_a: &mut Vec<u8>,
+                ser_b: &mut Vec<u8>
+            ) -> arbitrary::Result<Option<Ordering>> {
+                let mut type_id = self.0;
+                $(
+                    if type_id == 0 {
+                        let a: $ty = serialize_type(data, ser_a)?;
+                        let b: $ty = serialize_type(data, ser_b)?;
+                        return Ok(a.partial_cmp(&b));
+                    }
+                    type_id -= 1;
+                )+
+                unreachable!()
+            }
+        }
+        impl std::fmt::Debug for Single {
+            #[allow(unused_assignments)]
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                let mut type_id = self.0;
+                $(
+                    if type_id == 0 {
+                        return write!(f, "{}", stringify!($ty));
+                    }
+                    type_id -= 1;
+                )+
+                unreachable!()
+            }
+        }
+        impl<'a> Arbitrary<'a> for Single {
+            fn arbitrary(data: &mut Unstructured<'a>) -> arbitrary::Result<Self> {
+                let type_id = data.arbitrary()?;
+                if type_id < count!($($ty)*) as u16 {
+                    Ok(Single(type_id))
+                } else {
+                    Err(arbitrary::Error::IncorrectFormat)
                 }
-                type_id -= 1;
-            )+
-            Err(arbitrary::Error::IncorrectFormat)
+            }
         }
     }
 }
@@ -45,7 +78,7 @@ gen_single_fns!(u8 u16 u32 u64 u128 usize i8 i16 i32 i64 i128 isize f32 f64 Stri
 
 #[derive(Debug, Arbitrary)]
 enum TypeDef {
-    Single(u16),
+    Single(Single),
     Vec(Box<TypeDef>),
     Tuple(Vec<TypeDef>),
     Enum(Vec<(i64, TypeDef)>),
@@ -58,7 +91,7 @@ fn test_typedef(
     ser_b: &mut Vec<u8>,
 ) -> arbitrary::Result<Option<Ordering>> {
     match typedef {
-        TypeDef::Single(type_id) => test_single(*type_id, data, ser_a, ser_b),
+        TypeDef::Single(single) => single.test_single(data, ser_a, ser_b),
         _ => Err(arbitrary::Error::IncorrectFormat),
     }
 }
